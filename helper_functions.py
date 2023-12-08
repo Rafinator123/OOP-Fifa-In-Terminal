bdd3c7b09d6fba9e5355ae400dab6eec19204ca4
as13909 <102133164+as13909@users.noreply.github.com>
2023-12-07 22:29:49 -0500

    added functions and changed things around

--- helper_functions.py
+++ helper_functions.py
@@ -1,7 +1,57 @@
 """Includes helper functions"""
-def calculateGameCPU(t1, t2):
+import random
+from pyfiglet import Figlet
+
+used_colors = list()
+
+def scoreCalc(offense, defense):
+    return max(0, offense - defense)
+
+def calculateGameCPU_group_stages(t1, t2):
+    t1_score = scoreCalc(random.randint(t1.offense-2,t1.offense+2), random.randint(t2.defense-2, t2.offense+2))
+    t2_score = scoreCalc(random.randint(t2.offense-2,t2.offense+2), random.randint(t1.defense-2, t1.offense+2))
+    if t1_score == t2_score: 
+        return [t1,t2]
+    elif t1_score > t2_score: 
+        return [t1]
+    else:
+        return [t2]
+    
+def calculateGameCPU_knockouts(t1, t2):
     """Calculates games between teams"""
-    return
+    
+    t1_score = scoreCalc(random.randint(t1.offense-2,t1.offense+2), random.randint(t2.defense-2, t2.offense+2))
+    t2_score = scoreCalc(random.randint(t2.offense-2,t2.offense+2), random.randint(t1.defense-2, t1.offense+2))
+    #Extra time
+    if t1_score == t2_score:
+        t1_score = scoreCalc(random.randint(t1.offense-2,t1.offense), random.randint(t2.defense-2, t2.offense))
+        t2_score = scoreCalc(random.randint(t2.offense-2,t2.offense), random.randint(t1.defense-2, t1.offense))
+        if t1_score > t2_score:
+            return t1
+        elif t1_score < t2_score:
+            return t2
+    else:
+        if t1_score > t2_score:
+            return t1
+        else:
+            return t2
+    # Penalties
+    t1_penalties = 0
+    t2_penalties = 0
+
+    while t1_penalties < 5 and t2_penalties < 5:
+        t1_penalty_score = random.randint(0, 1)
+        t2_penalty_score = random.randint(0, 1)
+
+        if t1_penalty_score > t2_penalty_score:
+            t1_penalties += 1
+        else:
+            t2_penalties += 1
+
+    if t1_penalties > t2_penalties:
+        return t1
+    else:
+        return t2
 
 def calculateGameUser(opposition): 
     """Calculates game between user and opposition"""
@@ -11,11 +61,27 @@ def generateQuestion():
     """Returns question:answer paring."""
     return
 
-def groupStages():
-    return
-def quarterFinals():
-    return
-def semiFinals():
-    return
-def finals():
-    return
+def stars(num_starz):
+    return '* '*num_starz
+
+def isNum(inp):
+    for i in inp:
+        if not i.isdigit():
+            return False
+    return True
+
+
+def checkValidInputInt(upperLim,lowerLim,inputQuestion):
+    #upper lim and lower lim are INCLUSIVE
+    loop=True
+    while(loop):
+        out=input(inputQuestion)
+        if len(out)>0 and isNum(out) and int(out)<=upperLim and int(out)>=lowerLim:
+            loop=False
+            return out
+        else:
+            print("Invalid Input, try again")
+
+def printGoal():
+    f = Figlet(font='slant')
+    print(f.renderText('GOAL!!!'))
--- main.py
+++ main.py
@@ -1,55 +1,91 @@
 """Calls main function"""
 from pyfiglet import Figlet
 import random
-import names
 import time
+from world_cup import WorldCup, Group
+from helper_functions import *
+
 
 #this is for introduction. I.e. each time you run game it will say you had an illustrious carrier at one of these 3
 random_soccer_teams = [
-    "FC Barcelona",
-    "Manchester United FC",
+    "AC Milan",
     "Juventus FC",
+    "Internazionale",
+    
+    "FC Barcelona",
+    "Real Madrid",
+    "Atletico de Madrid",
+
+    "Manchester United",
+    "Manchester City",
+    "Liverpool",
+    
     "Bayern Munich",
-    "Paris Saint-Germain FC",
-    "Boca Juniors",
-    "Ajax Amsterdam",
-    "Flamengo",
     "Borussia Dortmund",
-    "Celtic FC"
 ]
 
 
-def isNum(inp):
-    if len(inp)==0: return False
-    for i in inp:
-        if not i.isdigit():
-            return False
-    return True
-
-
-def checkValidInputInt(upperLim,lowerLim,inputQuestion):
-    #upper lim and lower lim are INCLUSIVE
-    loop=True
-    while(loop):
-        out=input(inputQuestion)
-        if isNum(out) and int(out)<=upperLim and int(out)>=lowerLim:
-            loop=False
-            return out
-        else:
-            print("Invalid Input, try again")
-
-
 def main():
+    print("----------------------------------------------------------------------------------")
+    f = Figlet(font='slant')
+    print(f.renderText('Welcome to PyFIFA!'))
+    print("----------------------------------------------------------------------------------")
+    print('''
+PyFIFA is an interactive text-based soccer game inspired by the 2023 FIFA World 
+Cup in Qatar. Please press the Enter/Return button on your keyboard to start the 
+game and initate character creation. We hope you enjoy!
+
+This package was created by:
+Alex Hmitti
+Rafael-Nadal Scala
+Aaron Stein
+
+Github:
+https://github.com/Rafinator123/OOP-Fifa-In-Terminal
+''')
+    print("----------------------------------------------------------------------------------")
+    input() #waits for user to click 
+    print("Character Creation:")
+    f_name = input("What is your first name? ")
+    l_name = input("What is your last name? ")
+    print("Coach's Name:",f_name,l_name)
+
+    print(f'''\nAfter an ilustrious career working at {random_soccer_teams[random.randint(0,len(random_soccer_teams)-1)]}, you've been approached 
+from the following clubs to manage them at the world cup.\n''')
+          
+    time.sleep(3)
+
+    ratings=["Easy","Medium","Hard","Very Hard"]
+    #Print four teams here going from easy difficulty to hard:
+    wc = WorldCup()
+    randomized_team_selection =[]
+    randomized_team_selection.append(wc.pot1[random.randint(0, len(wc.pot1)-1)])
+    randomized_team_selection.append(wc.pot2[random.randint(0, len(wc.pot2)-1)])
+    randomized_team_selection.append(wc.pot3[random.randint(0, len(wc.pot3)-1)])
+    randomized_team_selection.append(wc.pot4[random.randint(0, len(wc.pot4)-1)])
     
+    for i in range(0,4):
+        print(f"\n {i + 1}. \n {randomized_team_selection[i].nation}\n--------------\n\
+        Challenge Level: {ratings[i]} \nOFFENSE: {stars(randomized_team_selection[i].offense)}\nDEFENSE: {stars(randomized_team_selection[i].defense)}")
+        time.sleep(2)
+             
     print()
-    checkValidInputInt(4,1,"Choose a team: ")
-    print("\nCurrent Lineup:\n--------------\nCoach:",f_name,l_name+"\n--------------")
-    positions = ["GK","LB","CB","CB","RB","CM","CM","CAM","LW","ST","RW"]
-    for i in positions:
-        print(i+":",names.get_full_name(gender="male"))
-        time.sleep(.25)
+    choice = checkValidInputInt(4,1,"Choose your team: ")
+    player_nation = randomized_team_selection[int(choice)-1]
+            
+    seeOthers = checkValidInputInt(2,1,"\nWould you like to see the results of the other nations? Type 1 for 'YES' and 2 for 'NO': ")
+    #Simulate world cup
+    #! Group stages
+    wc.groupStages()
+    if(int(seeOthers)==1):
+        wc.printGS()
+    #! Round of 16
+    
+    #!Quarter finals
+    
+    #!Semi-finals
+    
+    #!Finals
+#main()
 
-
-
-
-main()
+printGoal()
